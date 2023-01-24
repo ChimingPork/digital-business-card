a2791731ada0acf1354912c74cfb18a28cfe6b18
Josh Bellingham <92236549+ChimingPork@users.noreply.github.com>
2023-01-23 20:00:17 -0800

    Final Draft of Text and layout

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,10 +3,17 @@ export default function Main () {
         <main className="Main">
             <div className="Main-content">
                 <h3>About</h3>
-                <p>Placeholder text lets see what happens if I cross this border. 
-                    Placeholder text lets see what happens if I cross this border</p>
-                <h3>Proficiencies</h3>
-                <p>Stuff I like, when this get's bigger it should fill into the Footer I bet </p>
+                <p>Hi, I'm Josh, a Frontend Developer and Google Analytics Consultant from North Vancouver. 
+                    I'm an enthusiastic builder with a passion for problem solving and bringing projects to life.
+                    In addition, I'm passionate about helping people see those projects reach the audiences they were built for.
+                    My experience in Google Analytics and Google Tag Manager helps me get client's the insights they need for their projects to suceed.
+                </p>
+                <br></br>
+                <h3>Proficiencies in Development</h3>
+                <p>React, Javascript ES6, HTML 5, CSS, Node.js,, Solidity (familiar with), Unity (familiar with). </p>
+                <br></br>
+                <h3>Proficiencies in Google Analytics</h3>
+                <p>Google Analytics 4 (GA4), Universal Analytics to GA4 Migration, Google Tag Manager, Google Data Studio. </p>
             </div>
         </main>
     )
